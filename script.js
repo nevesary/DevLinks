901ba7083b0a8201e698fd9e07bf3885b496cfb1
Aryane <aryane.rochaneves33@gmail.com>
2023-09-07 15:22:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle('light')
+
+  // Pegar a tag img
+  const img = document.querySelector('#profile img')
+
+  //Substituir a imagem
+  if(html.classList.contains('light')){
+
+    //Se tiver light mode, adicionar a imagem light
+    img.setAttribute('src', './assets/assets/ary2.jpeg')
+  }else{
+
+    //Se tiver algum light mode manter a imagem normal
+    img.setAttribute('src', './assets/assets/ary.jpeg')
+
+  }
+}
